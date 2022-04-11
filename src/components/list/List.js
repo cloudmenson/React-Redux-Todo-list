85a94e90498e1acde83d4226b0fc9672d0ff0ef2
Danylo Hrytsenko <74321926+cloudmenson@users.noreply.github.com>
2022-04-11 11:49:27 +0300

    Add files via upload

---
+++ src/components/list/List.js
@@ -0,0 +1,35 @@
+import "./List.css";
+import { useDispatch, useSelector } from "react-redux";
+import { onDeleteTodo, onUpdateTodo } from "../../store/actions/todo";
+
+export default function List() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  return (
+    <div className="App__todo-list">
+      {todos.map((todos) => {
+        return (
+          <div
+            className={
+              todos.completed
+                ? "App__todo-list--item done"
+                : "App__todo-list--item"
+            }
+            key={todos.id}
+            onClick={() => dispatch(onUpdateTodo(todos.id))}
+          >
+            {todos.title}
+            <div>
+              <button
+                className="App__todo-list-remove-btn waves-effect waves-light btn red"
+                onClick={() => dispatch(onDeleteTodo(todos.id))}
+              >
+                <i className="material-icons">remove</i>
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
